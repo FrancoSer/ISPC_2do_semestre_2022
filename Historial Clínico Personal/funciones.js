e82507a483daa450873c1aec3a8e953c29a83611
NicoLoreto <nicoloreto2@gmail.com>
2022-11-01 20:29:31 -0300

    agrego funcionalidad js para validar cuil

--- Historial Clínico Personal/funciones.js
+++ Historial Clínico Personal/funciones.js
@@ -1,7 +1,17 @@
 
 // comprobar contrasenas
+// restricciones de numero de cuil
 
-function comprobarContrasena(){
+function comprobarContrasenaYcuil(){
+    let cuil = document.getElementById('cuil').value
+    let cuilStr = cuil.toString()
+    if (cuilStr.length != 11){
+        alert ('El número ingresado no corresponde a un número de cuil')
+        console.log ('El número ingresado no corresponde a un número de cuil')
+    }else{
+        console.log ('Cuil validado')
+    }
+
     let clave1 = document.getElementById('crearContrasena').value;
     let clave2 = document.getElementById ('confirmarContrasena').value;
 
@@ -14,3 +24,23 @@ function comprobarContrasena(){
     }
 }
 
+
+
+// restricciones de numero de cuil
+
+/*function restriccionCuil(){
+    let cuil = document.getElementById('cuil').value
+    let cuilStr = cuil.toString()
+    if (cuilStr.length != 11){
+        alert ('El número ingresado no corresponde a un número de cuil')
+        console.log ('El número ingresado no corresponde a un número de cuil')
+    }else{
+        console.log ('Cuil validado')
+    }
+}
+
+restriccionCuil()
+
+
+// restricciones de minimo de letras para nombre
+*/
