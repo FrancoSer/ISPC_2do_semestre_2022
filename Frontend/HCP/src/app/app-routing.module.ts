e53ab54ec6c6b69584911d33d55a873ec3d6b9c4
NicoLoreto <nicoloreto2@gmail.com>
2023-05-02 02:06:35 -0300

    routes for child added

--- Frontend/HCP/src/app/app-routing.module.ts
+++ Frontend/HCP/src/app/app-routing.module.ts
@@ -3,10 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ErrorPageComponent } from './shared/pages/error/error-page.component';
 import { InicioComponent } from './shared/pages/inicio/inicio.component';
-import { UMLoginComponent } from './auth/UM/UM-login/UM-login.component';
-import { UMRegistroComponent } from './auth/UM/UM-registro/UM-registro.component';
-import { UPLoginComponent } from './auth/UP/UP-login/UP-login.component';
-import { UPRegistroComponent } from './auth/UP/UP-registro/UP-registro.component';
 import { UMHomeComponent } from './UM/pages/home/UM-home.component';
 import { UPHomeComponent } from './UP/pages/home/UP-home.component';
 import { SobreNosotrosComponent } from './shared/pages/sobre-nosotros/sobre-nosotros.component';
@@ -20,25 +16,10 @@ const routes: Routes = [
   },
 
   {
-    path: 'login-up',
-    component: UPLoginComponent
-  },
-  {
-    path: 'registro-up',
-    component: UPRegistroComponent
-  },
-  {
     path: 'home-up',
     component: UPHomeComponent
   },
-  {
-    path: 'login-um',
-    component: UMLoginComponent
-  },
-  {
-    path: 'registro-um',
-    component: UMRegistroComponent
-  },
+
   {
     path: 'home-um',
     component: UMHomeComponent
--- Frontend/HCP/src/app/auth/auth.module.ts
+++ Frontend/HCP/src/app/auth/auth.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppRoutingModule } from '../app-routing.module';
 
+import { UPLoginComponent } from './UP/UP-login/UP-login.component';
+import { UMLoginComponent } from './UM/UM-login/UM-login.component';
+import { UMRegistroComponent } from './UM/UM-registro/UM-registro.component';
+import { UPRegistroComponent } from './UP/UP-registro/UP-registro.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UMLoginComponent,
+    UMRegistroComponent,
+    UPLoginComponent,
+    UPRegistroComponent
+
+  ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AuthRoutingModule
+    
   ]
 })
 export class AuthModule { }
