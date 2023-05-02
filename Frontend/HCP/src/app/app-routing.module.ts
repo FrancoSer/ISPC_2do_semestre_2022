4ca264f39cc2a79efa850b9296ba74b45b0cbfc7
NicoLoreto <nicoloreto2@gmail.com>
2023-05-02 02:24:04 -0300

    lazyload on auth-routing.module

--- Frontend/HCP/src/app/app-routing.module.ts
+++ Frontend/HCP/src/app/app-routing.module.ts
@@ -16,6 +16,11 @@ const routes: Routes = [
   },
 
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+
+  {
     path: 'home-up',
     component: UPHomeComponent
   },
