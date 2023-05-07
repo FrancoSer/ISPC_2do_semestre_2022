3f64f6872ddb0a36b7d52d44e57a25d7b1910157
Cristian López <cristianlp.ok@gmail.com>
2023-05-07 19:09:08 -0300

    Se añadió el UPHomeComponent

--- Frontend/HCP/src/app/app.module.ts
+++ Frontend/HCP/src/app/app.module.ts
@@ -6,10 +6,19 @@ import { SharedModule } from './shared/shared.module';
 import { AuthModule } from './auth/auth.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { UPHomeComponent } from './UP/pages/home/UP-home.component';
+import { PlanPremiumComponent } from './UP/shop/plan-premium/plan-premium.component';
+import { UMModule } from './UM/um.module';
+
+
+
+
 
 @NgModule({
   declarations: [	
     AppComponent,
+    UPHomeComponent,
+    PlanPremiumComponent
    
    ],
   imports: [
@@ -19,6 +28,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    UMModule
   ],
   providers: [],
   bootstrap: [AppComponent]
