83c135ba2ad008a7da6374fac751e80cdaad0df8
Cristian López <cristianlp.ok@gmail.com>
2023-05-07 19:05:39 -0300

    Se creó componente UM-Agenda

---
+++ Frontend/HCP/src/app/UM/pages/agenda/um-agenda.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-um-agenda',
+  templateUrl: './um-agenda.component.html',
+  styleUrls: ['./um-agenda.component.css']
+})
+export class UMAgendaComponent {
+
+}
