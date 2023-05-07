ccd00a8849102143d16247201c86d232168c464f
Cristian López <cristianlp.ok@gmail.com>
2023-05-07 19:06:18 -0300

    Update UM-Home

--- Frontend/HCP/src/app/UM/pages/home/UM-home.component.ts
+++ Frontend/HCP/src/app/UM/pages/home/UM-home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-UM-home',
   templateUrl: './UM-home.component.html',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UMHomeComponent implements OnInit {
 
+  sideNavStatus:boolean=true;
   constructor() { }
 
   ngOnInit() {
+
   }
 
-}
+   
+  }
+  
+
+
+
+
