2d35115acee047243e046c960bb835a2681ec732
DiegoGuardatti <dguardatti@gmail.com>
2023-05-07 09:39:02 -0300

    se creo componente footer en shared

--- Frontend/HCP/src/app/shared/shared.module.ts
+++ Frontend/HCP/src/app/shared/shared.module.ts
@@ -4,12 +4,14 @@ import { ErrorPageComponent } from './pages/error/error-page.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { SobreNosotrosComponent } from './pages/sobre-nosotros/sobre-nosotros.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     ErrorPageComponent,
     InicioComponent,
     SobreNosotrosComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
