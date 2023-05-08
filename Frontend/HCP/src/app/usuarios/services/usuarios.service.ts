3a08a100731b6a564f8ea23952e227612f920a55
NicoLoreto <nicoloreto2@gmail.com>
2023-05-08 03:39:25 -0300

    baseUrl created on service

--- Frontend/HCP/src/app/usuarios/services/usuarios.service.ts
+++ Frontend/HCP/src/app/usuarios/services/usuarios.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environment/envaironment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
+
+  private baseUrl = environment.baseUrl
 
 constructor() { }
 
