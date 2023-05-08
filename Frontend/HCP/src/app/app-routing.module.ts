260310cade5bef13876febece4f0e2c137ab8226
NicoLoreto <nicoloreto2@gmail.com>
2023-05-08 03:35:13 -0300

    shop component deleted

--- Frontend/HCP/src/app/app-routing.module.ts
+++ Frontend/HCP/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ErrorPageComponent } from './shared/pages/error/error-page.component';
 import { InicioComponent } from './shared/pages/inicio/inicio.component';
-import { UMHomeComponent } from './UM/pages/home/UM-home.component';
-import { UPHomeComponent } from './UP/pages/home/UP-home.component';
+
 import { SobreNosotrosComponent } from './shared/pages/sobre-nosotros/sobre-nosotros.component';
+import { UPHomeComponent } from './usuarios/UP/pages/home/UP-home.component';
+import { UMHomeComponent } from './usuarios/UM/pages/home/UM-home.component';
 
 const routes: Routes = [
 
