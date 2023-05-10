06c05c87ef2b2d72f09d9dfc9a46e17755651576
NicoLoreto <nicoloreto2@gmail.com>
2023-05-10 20:05:36 -0300

    router plan-premium added

--- Frontend/HCP/src/app/users/users-routing.module.ts
+++ Frontend/HCP/src/app/users/users-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { UPHomeComponent } from './UP/pages/home/UP-home.component';
 import { UMHistorialComponent } from './UM/pages/historial/um-historial.component';
 import { UMHomeComponent } from './UM/pages/home/UM-home.component';
+import { PlanPremiumComponent } from './UP/shop/plan-premium/plan-premium.component';
 
 const routes: Routes = [
 
@@ -10,7 +11,14 @@ const routes: Routes = [
     path: 'home-up',
     component: UPHomeComponent,
     children: [
-      { path: 'up-historial', component: UMHistorialComponent }
+      { 
+        path: 'up-historial', 
+        component: UMHistorialComponent,
+      },
+      {
+        path: 'plan-premium',
+        component: PlanPremiumComponent
+      }
     ]
   },
 
