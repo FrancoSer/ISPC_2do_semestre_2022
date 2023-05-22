ff7cd65077d3681ec9a10a598536fb6aed98ba83
Gaston <paez.gastonm@gmail.com>
2023-05-21 23:57:13 -0300

    Sprint 2. Register Paciente in DjangoAdmin

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Paciente(models.Model):
+    cuil = models.CharField(max_length=100)
+    name_p = models.CharField(max_length=100)
+    nacimiento = models.DateField(max_length=100)
+    apellido_p = models.CharField(max_length=100)
+    telefono_p = models.CharField(max_length=100)
+    direccion_p = models.CharField(max_length=100)
+    mail_p = models.CharField(max_length=100)
+    genero_p = models.CharField(max_length=100)
+    password_p = models.CharField(max_length=100)
+    premium_p = models.CharField(max_length=100)
+    mail_p = models.CharField(max_length=100)
+  
