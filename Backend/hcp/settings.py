a9f175630b03cc53e63aca6b09756444ad7527e2
NicoLoreto <nicoloreto2@gmail.com>
2023-05-31 02:14:55 -0300

    backend cloned

--- Backend/hcp/settings.py
+++ Backend/hcp/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = "users.CustomUser"
 
 
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
