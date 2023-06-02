b6bb24ff3feea052702af72f105069add5d302f3
NicoLoreto <nicoloreto2@gmail.com>
2023-06-02 00:10:55 -0300

    formReactive pacienteRegistro created

--- Frontend/HCP/src/app/auth/UP/UP-registro/UP-registro.component.ts
+++ Frontend/HCP/src/app/auth/UP/UP-registro/UP-registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Genero } from 'src/app/users/interfaces/interfaces';
 
 @Component({
   selector: 'app-UP-registro',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UPRegistroComponent implements OnInit {
 
+  public pacienteRegistro = new FormGroup({
+
+    nombre:         new FormControl<string>(''),
+    apellido:       new FormControl<string>(''),
+    cuil:           new FormControl<string>(''),
+    mail:           new FormControl<string>(''),
+    password:       new FormControl<string>(''),
+    passwordRepet:  new FormControl<string>(''),
+    nacimiento:     new FormControl<string>(''),
+    genero:         new FormControl<Genero>(Genero.masculino),
+    grupoSanguineo: new FormControl<string>(''),
+    
+  });
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
 
     
   }
