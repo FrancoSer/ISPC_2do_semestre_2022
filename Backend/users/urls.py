e98204adf3fabec3765d0b0350fbba291ba41a37
Gaston Paez <paez.gastonm@gmail.com>
2023-06-06 15:47:13 -0300

    add login, signup Paciente and Medico models, with serializers and routes

--- Backend/users/urls.py
+++ Backend/users/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path, include
-from .views import LoginView
+# from .views import LoginView
 from . import views
 from rest_framework import routers
+from .views import PacienteSignupView, MedicoSignupView
+from .views import PacienteLoginView, MedicoLoginView
 
 router=routers.DefaultRouter()
 router.register(r'paciente',views.PacienteViewSet)
+router.register(r'medico',views.MedicoViewSet)
 urlpatterns = [
-    path('auth/login/', LoginView.as_view(), name='auth_login'),
-    # path('auth/signup/', SignupView.as_view(), name='auth_signup'),
-    # path('auth/logout/', LogoutView.as_view(), name='auth_logout'),
+    path('paciente/signup/', PacienteSignupView.as_view(), name='paciente_signup'),
+    path('paciente/login/', PacienteLoginView.as_view(), name='paciente_login'),
+    path('medico/signup/', MedicoSignupView.as_view(), name='medico_signup'),
+    path('medico/login/', MedicoLoginView.as_view(), name='medico_login'),
     path('',include(router.urls)),
-    
-    
 ]
--- Backend/users/views.py
+++ Backend/users/views.py
@@ -1,42 +1,111 @@
-from django.contrib.auth import authenticate, login, logout
 from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import PacienteSerializer
-from rest_framework import generics, status
-from rest_framework.permissions import AllowAny
+from rest_framework import status
 from .models import Paciente
-from rest_framework import viewsets
+from .models import Medico
+from .serializers import PacienteSerializer
+from .serializers import MedicoSerializer
+from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth import logout
+from rest_framework import views
 
+# Register API ==> http://127.0.0.1:8000/api/paciente/signup/
+class PacienteSignupView(APIView):    
+    queryset = Paciente.objects.all()    
+    serializer_class = PacienteSerializer
+    def post(self, request):
+        serializer = PacienteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(password_p=make_password(request.data['password_p']))
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# Login API ==> http://127.0.0.1:8000/api/paciente/login/
+class PacienteLoginView(APIView):
+    def post(self, request):
+        cuil = request.data['cuil']
+        password = request.data['password_p']
+        paciente = Paciente.objects.get(cuil=cuil)
+        if check_password(password, paciente.password_p):
+            serializer = PacienteSerializer(paciente)
+            return Response(serializer.data)
+        return Response("Credenciales inválidas", status=status.HTTP_401_UNAUTHORIZED)
+
+# ==> http://127.0.0.1:8000/api/paciente/1/
 class PacienteViewSet(viewsets.ModelViewSet):
     queryset = Paciente.objects.all()
-    serializer_class = PacienteSerializer
+    serializer_class = PacienteSerializer 
     
     
-
-class LoginView(APIView):
-    permission_classes = [AllowAny] 
+# Register API ==> http://127.0.0.1:8000/api/medico/signup/
+class MedicoSignupView(APIView):    
+    queryset = Medico.objects.all()    
+    serializer_class = MedicoSerializer
     def post(self, request):
-        # Recuperamos las credenciales y autenticamos al usuario
-        cuil = request.data.get('cuil', None)
-        password = request.data.get('password_p', None)
-        user = authenticate(cuil=cuil, password=password)
-        # Si es correcto añadimos a la request la información de sesión
-        if user:
-            login(request, user)
-            return Response(
-                PacienteSerializer(user).data,
-                status=status.HTTP_200_OK)
-        # Si no es correcto devolvemos un error en la petición
-        return Response(
-            status=status.HTTP_404_NOT_FOUND)
+        serializer = MedicoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(password_m=make_password(request.data['password_m']))
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Login API ==> http://127.0.0.1:8000/api/medico/login/
+class MedicoLoginView(APIView):
+    def post(self, request):
+        matricula = request.data['matricula']
+        password = request.data['password_m']
+        medico = Medico.objects.get(matricula=matricula)
+        if check_password(password, medico.password_m):
+            serializer = MedicoSerializer(medico)
+            return Response(serializer.data)
+        return Response("Credenciales inválidas", status=status.HTTP_401_UNAUTHORIZED)
+
+# ==> http://127.0.0.1:8000/api/Medico/1/
+class MedicoViewSet(viewsets.ModelViewSet):
+    queryset = Medico.objects.all()
+    serializer_class = MedicoSerializer 
+
+
+
+class LogoutView(views.APIView):
+    def post(self, request):
+        logout(request)
+        return Response({'message': "Logout successful"})
+####################################################################
+############################## OLD #################################
+####################################################################
     
+## Vistas antiguas
+
 # class LogoutView(APIView):
 #     def post(self, request):
 #         logout(request)
 
 #         return Response(status=status.HTTP_200_OK)
 
-# class SignupView(generics.CreateAPIView):
-#     serializer_class = UserSerializer
+# class LoginView(APIView):
+#     permission_classes = [AllowAny]
+#     def post(self, request):
+#         # Recuperamos las credenciales y autenticamos al usuario
+#         cuil = request.data.get('cuil', None)
+#         password = request.data.get('password_p', None)
+#         user = authenticate(cuil=cuil, password=password)
+#         # Si es correcto añadimos a la request la información de sesión
+#         if user:
+#             login(request, user)
+#             return Response(
+#                 PacienteSerializer(user).data,
+#                 status=status.HTTP_200_OK)
+#         # Si no es correcto devolvemos un error en la petición
+#         return Response(
+#             status=status.HTTP_404_NOT_FOUND)
+    
+# # class LogoutView(APIView):
+# #     def post(self, request):
+# #         logout(request)
+
+# #         return Response(status=status.HTTP_200_OK)
+
+# # class SignupView(generics.CreateAPIView):
+# #     serializer_class = UserSerializer
