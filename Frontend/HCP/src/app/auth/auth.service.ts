b1a8eb9928cf1478b9ed398e1ae0dae2950d8298
NicoLoreto <nicoloreto2@gmail.com>
2023-06-08 20:35:52 -0300

    pull backend new endpoints

--- Frontend/HCP/src/app/auth/auth.service.ts
+++ Frontend/HCP/src/app/auth/auth.service.ts
@@ -5,43 +5,48 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environment/envaironment';
 import { MedicoRegistro, PacienteRegistro } from './interfaces/auth';
 
-@Injectable({
+@Injectable( {
   providedIn: 'root'
-})
-export class AuthService {
+} )
+export class AuthService
+{
 
-private baseUrl = environment.baseUrl
+  private baseUrl = environment.baseUrl;
 
-constructor(private http: HttpClient) { }
+  constructor ( private http: HttpClient ) { }
 
-registroPaciente(paciente: PacienteRegistro): Observable<Paciente>{
-  console.log(`este es el error ${(paciente)}`)
-  return this.http.post<Paciente>(`${this.baseUrl}/api/auth/signup/`, paciente)
+  registroPaciente ( paciente: PacienteRegistro ): Observable<Paciente>
+  {
+    console.log( `este es el error ${ ( paciente ) }` );
+    return this.http.post<Paciente>( `${ this.baseUrl }/api/auth/signup/`, paciente );
 
-}
-
-registroMedico(medico: MedicoRegistro): Observable<Medico>{
-  return this.http.post<Medico>(`${this.baseUrl}/api/auth/signup/`, medico)
-
-}
-
-loginPaciente(email: string, password: string){
-
-  const body = {
-    email: email,
-    password: password
   }
-  return this.http.post(`${this.baseUrl}/api/auth/login/`, body)
-}
 
-loginMedico(username: string,email: string, password: string){
+  registroMedico ( medico: MedicoRegistro ): Observable<Medico>
+  {
+    return this.http.post<Medico>( `${ this.baseUrl }/api/auth/signup/`, medico );
 
-  const body = {
-    username: username,
-    email: email,
-    password: password
   }
-  return this.http.post(`${this.baseUrl}/api/auth/login/`, body)
-}
+
+  loginPaciente ( email: string, password: string )
+  {
+
+    const body = {
+      email: email,
+      password: password
+    };
+    return this.http.post( `${ this.baseUrl }/api/auth/login/`, body );
+  }
+
+  loginMedico ( username: string, email: string, password: string )
+  {
+
+    const body = {
+      username: username,
+      email: email,
+      password: password
+    };
+    return this.http.post( `${ this.baseUrl }/api/auth/login/`, body );
+  }
 
 }
