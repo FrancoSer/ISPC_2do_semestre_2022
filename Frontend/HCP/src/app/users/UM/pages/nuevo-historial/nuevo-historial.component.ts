d3215096763db27b52958470f39cfeaff3356113
NicoLoreto <nicoloreto2@gmail.com>
2023-06-09 22:35:50 -0300

    reactive form  HC created

--- Frontend/HCP/src/app/users/UM/pages/nuevo-historial/nuevo-historial.component.ts
+++ Frontend/HCP/src/app/users/UM/pages/nuevo-historial/nuevo-historial.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
-@Component({
+@Component( {
   selector: 'app-nuevo-historial',
   templateUrl: './nuevo-historial.component.html',
-  styleUrls: ['./nuevo-historial.component.css']
-})
-export class NuevoHistorialComponent implements OnInit {
+  styleUrls: [ './nuevo-historial.component.css' ]
+} )
+export class NuevoHistorialComponent implements OnInit
+{
 
-  constructor() { }
+  private form = new FormGroup( {
 
-  ngOnInit() {
+    referencia_p: new FormControl<string>( '' ),
+    especialidad: new FormControl<string>( '' ),
+    diagnositico: new FormControl<string>( '' ),
+    antecedentes: new FormControl<string>( '' ),
+    presion: new FormControl<string>( '' ),
+    altura: new FormControl<string>( '' ),
+    prueba_diagnostica: new FormControl<string>( '' ),
+    medicacion: new FormControl<string>( '' ),
+    dieta: new FormControl<string>( '' ),
+    vacuna: new FormControl<string>( '' ),
+    proxima_visita: new FormControl<string>( '' ),
+    observaciones: new FormControl<string>( '' ),
+    adjunto: new FormControl<string>( '' )
+
+  } );
+
+  constructor () { }
+
+  ngOnInit ()
+  {
   }
 
 }
